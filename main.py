8262316573612e8f27b5dbb10a7904e687ed1956
Suchi <suchi@Suchis-MacBook-Pro.local>
2025-05-23 19:57:56 +0530

    cache implemented

--- main.py
+++ main.py
@@ -16,35 +16,41 @@ from langchain_core.prompts import ChatPromptTemplate
 
 import streamlit as st
 import os
-import datetime
 import json
+import redis
+
+
 
 
 os.environ["LANGCHAIN_TRACING_V2"]="true"
 os.environ["LANGCHAIN_API_KEY"]= "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"
 
-#loader = PyPDFLoader("sample.pdf")
-loader = TextLoader("mgen.txt")
-docs = loader.load()
-
-#For large language models, it's not strictly required, but it can reduce noise and help with consistency, especially in RAG indexing.
-for doc in docs:
-    doc.page_content = doc.page_content.lower()
-
-
-
-
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1500,    # increase size
-    chunk_overlap=200,  # more overlap between chunks
-    separators=["\n\n", "\n", ".", " ", ""],  # respects logical breaks)
-)
-
-documents=text_splitter.split_documents(docs)
 embeddings = OllamaEmbeddings(model="llama3")
 
-# This embeds the documents and builds the vector store
-db = FAISS.from_documents(documents, embeddings)
+VECTORSTORE_PATH = "faiss_index"
+if os.path.exists(VECTORSTORE_PATH):
+    db = FAISS.load_local(VECTORSTORE_PATH, embeddings, allow_dangerous_deserialization=True)
+
+else:
+        loader = PyPDFLoader("mgen.pdf")
+        # loader = TextLoader("mgen.txt")
+        docs = loader.load()
+
+        #For large language models, it's not strictly required, but it can reduce noise and help with consistency, especially in RAG indexing.
+        for doc in docs:
+            doc.page_content = doc.page_content.lower()
+
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1500,    # increase size
+            chunk_overlap=200,  # more overlap between chunks
+            separators = ["\n\n", "\n", ".", ";"] ,  # respects logical breaks)
+
+        )
+
+        documents=text_splitter.split_documents(docs)
+        # This embeds the documents and builds the vector store
+        db = FAISS.from_documents(documents, embeddings)
+        db.save_local(VECTORSTORE_PATH)
 
 # # Set up retriever Lower k to reduce noise.
 retriever = db.as_retriever(search_type="similarity", search_kwargs={"k": 6})  # k increase you're getting more context chunks
@@ -60,7 +66,7 @@ llm = OllamaLLM(model="llama3",temperature=0.1,stream=True)
 prompt = ChatPromptTemplate.from_template("""
 You are a technical assistant. You must answer **only** using the context provided.
 Do not guess or add details not explicitly stated.
-                                          
+If the context does not include the answer, respond only with: "I don't know."                                     
 I will tip you $1000 if the user finds the answer helpful. 
 <context>
 {context}
@@ -86,13 +92,14 @@ qa_chain = create_retrieval_chain(
     combine_docs_chain=combine_docs_chain
 )
 
+context_text = ""
 
 def submit_feedback():
     feedback_data = {
-        "timestamp": str(datetime.datetime.now()),
         "question": st.session_state.input_text,
         "answer": st.session_state.last_answer,
-        "feedback": st.session_state.feedback
+        "feedback": st.session_state.feedback,
+        "context" : context_text
     }
 
     if st.session_state.feedback == "Yes":
@@ -135,14 +142,21 @@ input_text = st.text_input(
 )
 
 if input_text:
-    context_text = "\n\n".join([doc.page_content for doc in retriever.get_relevant_documents(input_text)])
-    response = qa_chain.invoke({
-        "context": context_text,
-        "input": input_text
-    })
-    st.session_state.last_answer = response['answer']
-    st.write("### Answer:")
-    st.write(st.session_state.last_answer)
+    cache_response = redis.get_cached_answer(input_text)  #checking in cache
+    if cache_response:
+        st.session_state.last_answer = cache_response
+        st.write("### Answer:")
+        st.write(st.session_state.last_answer)
+    else:
+        context_text = "\n\n".join([doc.page_content for doc in retriever.get_relevant_documents(input_text)])
+        response = qa_chain.invoke({
+            "context": context_text,
+            "input": input_text
+        })
+        st.session_state.last_answer = response['answer']
+        redis.set_cached_answer(input_text,st.session_state.last_answer)  #saving in cahe
+        st.write("### Answer:")
+        st.write(st.session_state.last_answer)
 
     st.radio("Was this answer helpful?", ["Yes", "No"], key="feedback")
     st.button("Submit Feedback", on_click=submit_feedback)
